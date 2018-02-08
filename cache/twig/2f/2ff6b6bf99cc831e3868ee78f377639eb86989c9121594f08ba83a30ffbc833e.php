597fe0660f3f716210cee86bd5d84195b47c6d10
juniordawson <contact@stevendawson.co.uk>
2018-02-08 16:01:05 +0000

    Update favicon

--- cache/twig/2f/2ff6b6bf99cc831e3868ee78f377639eb86989c9121594f08ba83a30ffbc833e.php
+++ cache/twig/2f/2ff6b6bf99cc831e3868ee78f377639eb86989c9121594f08ba83a30ffbc833e.php
@@ -71,24 +71,85 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
         echo $this->getAttribute(($context["assets"] ?? null), "js", array(), "method");
         echo "
 
-    <link rel=\"icon\" href=\"";
-        // line 25
-        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/favicon.png");
+
+<link rel=\"apple-touch-icon\" sizes=\"57x57\" href=\"";
+        // line 26
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-57x57.png");
         echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"60x60\" href=\"";
+        // line 27
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-60x60.png");
+        echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"72x72\" href=\"";
+        // line 28
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-72x72.png");
+        echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"";
+        // line 29
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-76x76.png");
+        echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"114x114\" href=\"";
+        // line 30
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-114x114.png");
+        echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"";
+        // line 31
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-120x120.png");
+        echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"144x144\" href=\"";
+        // line 32
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-144x144.png");
+        echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"";
+        // line 33
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-152x152.png");
+        echo "\">
+<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"";
+        // line 34
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/apple-icon-180x180.png");
+        echo "\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"192x192\"  href=\"";
+        // line 35
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/android-icon-192x192.png");
+        echo "\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"";
+        // line 36
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/favicon-32x32.png");
+        echo "\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"96x96\" href=\"";
+        // line 37
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/favicon-96x96.png");
+        echo "\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"";
+        // line 38
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/favicon-16x16.png");
+        echo "\">
+<link rel=\"manifest\" href=\"";
+        // line 39
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/manifest.json");
+        echo "\">
+<meta name=\"msapplication-TileColor\" content=\"#ffffff\">
+<meta name=\"msapplication-TileImage\" content=\"";
+        // line 41
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->urlFunc("theme://img/ms-icon-144x144.png");
+        echo "\">
+<meta name=\"theme-color\" content=\"#ffffff\">
+
+
 
 </head>
 
 <body>
     
 ";
-        // line 31
+        // line 50
         $this->displayBlock('top', $context, $blocks);
-        // line 58
+        // line 77
         echo "
   ";
-        // line 59
+        // line 78
         $this->displayBlock('main', $context, $blocks);
-        // line 67
+        // line 86
         echo "
     
 
@@ -103,18 +164,18 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
         <div class=\"small-50 text-center\">
           <ul class=\"clean-list\">
               ";
-        // line 80
+        // line 99
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable($this->getAttribute($this->getAttribute(($context["pages"] ?? null), "children", array()), "visible", array()));
         foreach ($context['_seq'] as $context["_key"] => $context["page"]) {
-            // line 81
+            // line 100
             echo "                  <li>
                       <a href=\"";
-            // line 82
+            // line 101
             echo $this->getAttribute($context["page"], "url", array());
             echo "\">
                           ";
-            // line 83
+            // line 102
             echo $this->getAttribute($context["page"], "menu", array());
             echo "
                       </a>
@@ -124,7 +185,7 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['page'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 87
+        // line 106
         echo "          </ul>
         </div>
       </div>
@@ -137,17 +198,17 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
 </html>";
     }
 
-    // line 31
+    // line 50
     public function block_top($context, array $blocks = array())
     {
-        // line 32
+        // line 51
         echo "    <div class=\"brand\">
       <a href=\"";
-        // line 33
+        // line 52
         echo (((($context["base_url"] ?? null) == "")) ? ("/") : (($context["base_url"] ?? null)));
         echo "\">
         ";
-        // line 34
+        // line 53
         echo $this->getAttribute($this->getAttribute(($context["config"] ?? null), "site", array()), "title", array());
         echo "
       </a>
@@ -159,39 +220,39 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
           <div class=\"nav-mobile\"><a id=\"nav-toggle\" href=\"#!\"><span></span></a></div>
           <ul class=\"nav-list\">
             ";
-        // line 43
+        // line 62
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable($this->getAttribute($this->getAttribute(($context["pages"] ?? null), "children", array()), "visible", array()));
         foreach ($context['_seq'] as $context["_key"] => $context["page"]) {
-            // line 44
+            // line 63
             echo "              ";
             if ($this->getAttribute($context["page"], "visible", array())) {
-                // line 45
+                // line 64
                 echo "                ";
                 $context["current_page"] = ((($this->getAttribute($context["page"], "active", array()) || $this->getAttribute($context["page"], "activeChild", array()))) ? ("current") : (""));
-                // line 46
+                // line 65
                 echo "                <li class=\"";
                 echo ($context["current_page"] ?? null);
                 echo "\">
                     <a href=\"";
-                // line 47
+                // line 66
                 echo $this->getAttribute($context["page"], "url", array());
                 echo "\">
                         ";
-                // line 48
+                // line 67
                 echo $this->getAttribute($context["page"], "menu", array());
                 echo "
                     </a>
                 </li>
               ";
             }
-            // line 52
+            // line 71
             echo "            ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['page'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 53
+        // line 72
         echo "          </ul>
         </nav>
       </div>
@@ -199,25 +260,25 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
 ";
     }
 
-    // line 59
+    // line 78
     public function block_main($context, array $blocks = array())
     {
-        // line 60
+        // line 79
         echo "    <main>
 
         ";
-        // line 62
+        // line 81
         $this->displayBlock('content', $context, $blocks);
-        // line 64
+        // line 83
         echo "
     </main>
   ";
     }
 
-    // line 62
+    // line 81
     public function block_content($context, array $blocks = array())
     {
-        // line 63
+        // line 82
         echo "        ";
     }
 
@@ -233,7 +294,7 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
 
     public function getDebugInfo()
     {
-        return array (  221 => 63,  218 => 62,  212 => 64,  210 => 62,  206 => 60,  203 => 59,  195 => 53,  189 => 52,  182 => 48,  178 => 47,  173 => 46,  170 => 45,  167 => 44,  163 => 43,  151 => 34,  147 => 33,  144 => 32,  141 => 31,  128 => 87,  118 => 83,  114 => 82,  111 => 81,  107 => 80,  92 => 67,  90 => 59,  87 => 58,  85 => 31,  76 => 25,  71 => 23,  68 => 22,  65 => 21,  63 => 20,  58 => 18,  55 => 17,  52 => 16,  50 => 15,  46 => 13,  43 => 12,  41 => 11,  30 => 7,  22 => 1,);
+        return array (  282 => 82,  279 => 81,  273 => 83,  271 => 81,  267 => 79,  264 => 78,  256 => 72,  250 => 71,  243 => 67,  239 => 66,  234 => 65,  231 => 64,  228 => 63,  224 => 62,  212 => 53,  208 => 52,  205 => 51,  202 => 50,  189 => 106,  179 => 102,  175 => 101,  172 => 100,  168 => 99,  153 => 86,  151 => 78,  148 => 77,  146 => 50,  134 => 41,  129 => 39,  125 => 38,  121 => 37,  117 => 36,  113 => 35,  109 => 34,  105 => 33,  101 => 32,  97 => 31,  93 => 30,  89 => 29,  85 => 28,  81 => 27,  77 => 26,  71 => 23,  68 => 22,  65 => 21,  63 => 20,  58 => 18,  55 => 17,  52 => 16,  50 => 15,  46 => 13,  43 => 12,  41 => 11,  30 => 7,  22 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -270,7 +331,26 @@ class __TwigTemplate_53063351ae4813eefbd61f5ef348a081967be2755e1bb6bca9fc2da565c
 
       {{ assets.js() }}
 
-    <link rel=\"icon\" href=\"{{ url('theme://img/favicon.png') }}\">
+
+<link rel=\"apple-touch-icon\" sizes=\"57x57\" href=\"{{ url('theme://img/apple-icon-57x57.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"60x60\" href=\"{{ url('theme://img/apple-icon-60x60.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"72x72\" href=\"{{ url('theme://img/apple-icon-72x72.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"{{ url('theme://img/apple-icon-76x76.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"114x114\" href=\"{{ url('theme://img/apple-icon-114x114.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"{{ url('theme://img/apple-icon-120x120.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"144x144\" href=\"{{ url('theme://img/apple-icon-144x144.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"{{ url('theme://img/apple-icon-152x152.png') }}\">
+<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"{{ url('theme://img/apple-icon-180x180.png') }}\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"192x192\"  href=\"{{ url('theme://img/android-icon-192x192.png') }}\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"{{ url('theme://img/favicon-32x32.png') }}\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"96x96\" href=\"{{ url('theme://img/favicon-96x96.png') }}\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"{{ url('theme://img/favicon-16x16.png') }}\">
+<link rel=\"manifest\" href=\"{{ url('theme://img/manifest.json') }}\">
+<meta name=\"msapplication-TileColor\" content=\"#ffffff\">
+<meta name=\"msapplication-TileImage\" content=\"{{ url('theme://img/ms-icon-144x144.png') }}\">
+<meta name=\"theme-color\" content=\"#ffffff\">
+
+
 
 </head>
 
